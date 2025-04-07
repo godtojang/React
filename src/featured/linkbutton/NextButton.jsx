932ca909375f863793960f6f558030e47fcf1f1d
godtojang <90055406+godtojang@users.noreply.github.com>
2025-04-07 18:21:06 +0900

    Add files via upload

---
+++ src/featured/linkbutton/NextButton.jsx
@@ -0,0 +1,20 @@
+const NextButton = () => {
+  return (
+    <button
+      style={{
+        width: "300px",
+        border: "none",
+        padding: "15px",
+        textAlign: "center",
+        borderRadius: "4px",
+        fontWeight: "bold",
+        backgroundColor: "#e6e7e9",
+        color: "#b4b9bc",
+      }}
+    >
+      다음
+    </button>
+  );
+};
+
+export default NextButton;
